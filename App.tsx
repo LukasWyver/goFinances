d4081f3071ff443e8b3242ac7b4dec4e716d9f63
Lucas Gimenes <lucas_sin5@hotmail.com>
2023-02-10 13:01:10 -0300

    styled UserInfo on Header

--- App.tsx
+++ App.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
-import { View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from 'styled-components';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+
+} from '@expo-google-fonts/poppins'
+import * as SplashScreen from 'expo-splash-screen';
 
 import theme from './src/global/styles/theme';
 import Dashboard from './src/screens/Dashboard';
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+  
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  SplashScreen.hideAsync();
+
   return (
     <ThemeProvider theme={theme}>
       <StatusBar style="light" backgroundColor={theme.colors.primary} />          
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import { 
+  Container,
+   Header,
+   UserWrapper,
+   UserInfo,
+   Photo,
+   User,
+   UserGreeting,
+   UserName,
+   Icon,
+  } from './styles';
+
+
 
 export default function Dashboard() {
   return (
     <Container>
-        <Title>Hello World</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{uri: 'https://github.com/lukaswyver.png'}}/>
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Lucas</UserName>
+            </User>
+          </UserInfo>    
+          <Icon name="power"/>      
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
